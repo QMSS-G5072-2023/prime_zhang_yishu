66551ad58421478239bf6c0af370b19b7e19deb4
Yishu Zhang <yz4638@columbia.edu>
2023-10-27 22:54:55 -0400

    Utilize sphinx to auto-generate documentation
    It was not fully automated though

---
+++ prime_yz4638/docs/sphinx/_build/html/_sources/index.rst.txt
@@ -0,0 +1,49 @@
+.. prime_yz4638 documentation master file, created by
+   sphinx-quickstart on Fri Oct 27 22:38:32 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to prime_yz4638's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+prime_yz4638
+==============
+
+prime_yz4638 will check if your imput number is a prime number.
+More to come!
+
+Installation
+-----------------
+
+Install prime_yz4638 by running:
+
+    install prime_yz4638
+    
+Support
+------------
+
+If you are having issues, please let me know. yz4638@columbia.edu
+
+License
+------------
+
+The project is licensed under the MIT license.
+
+Credit
+----------
+
+This project is created with cookiecutter
---
+++ prime_yz4638/docs/sphinx/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = u"prime_yz4638"
+copyright = u"2023, Yishu Zhang"
+author = u"Yishu Zhang"
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+autoapi_dirs = ["../src"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
