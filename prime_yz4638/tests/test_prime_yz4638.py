54a9bda7d76a30a5fe12a99cff97d9f6eabd2ede
Yishu Zhang <yz4638@columbia.edu>
2023-10-31 17:05:40 -0400

    Introduce tests

--- prime_yz4638/tests/test_prime_yz4638.py
+++ prime_yz4638/tests/test_prime_yz4638.py
@@ -1,1 +1,16 @@
+import sys
+import os
+import pytest
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from prime_yz4638 import prime_yz4638
+
+import math
+
+
+@pytest.mark.parametrize("example, expected", [
+    (2, True), (3, True), (5, True), (7, True), (11, True),
+    (-3, False), (0, False), (1, False), (4, False), (6, False), 
+])
+def test_is_prime_param(example, expected):   
+    result = prime_yz4638.is_prime(example)
+    assert result == expected
